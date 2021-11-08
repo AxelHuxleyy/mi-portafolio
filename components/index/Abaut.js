230b7931ebe8fc279872df2b6d63a9da39b14f32
AXEL HUXLEY RAMIREZ SANCHEZ <axel_ra_s@hotmail.com>
2021-11-07 18:26:56 -0600

    Update Abaut.js

--- components/index/Abaut.js
+++ components/index/Abaut.js
@@ -5,7 +5,7 @@ const Abaut = () => {
         <div className="mx-4 mt-5 p-5 shadow-lg rounded-xl bg-red-600 opacity-90 lg:mx-24">
             <h1 className="text-center  text-black text-xl uppercase font-bold">About me</h1>
             <p className='text-justify text-black  mt-10'>
-            👋 ¡Hola! Soy Axel Huxley, recién egresado en ingeniería en desarrollo de software en la facultad de informática en la Universidad Autónoma de Querétaro.
+            👋 ¡Hola!  Axel Huxley, recién egresado en ingeniería en desarrollo de software en la facultad de informática en la Universidad Autónoma de Querétaro.
             </p>
             <p className="text-justify  text-black mt-3">
             🎓Durante esta etapa universitaria he desarrollado muchos proyectos tanto personales como académicos, en los cuales he podido desarrollar mis habilidades de comunicación, liderazgo, trabajo en equipo, tolerancia, como resolver y abordar los disantos problemas con los que me encontraba. Al mismo tiempo que estudiaba tome múltiples cursos por mi cuenta como podrían ser: master en javaScript, react js, react native, react avanzado: next js, graphql, apollo, tailwind, react hooks, oracle, unity. Además de realizar lo antes mencionado también realice un par de sitios web para unas empresas pymes.
