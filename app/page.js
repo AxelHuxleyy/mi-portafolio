40bcd2d5a0d34bdba53c1133dc02e02bf8398641
Axel Huxley <axel_ra_s@hotmail.com>
2026-05-07 14:29:18 -0600

    fuinal version

--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
-import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Skills from '../components/Skills';
 import Projects from '../components/Projects';
 import Experience from '../components/Experience';
-import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 import ScrollProgress from '../components/ScrollProgress';
 import ParticleField from '../components/ParticleField';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
